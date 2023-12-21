7a0e0c408c43d3b0d5a2d5e3e524c20b4fdeb638
Shanmugagouthaman <65980015+SG9822@users.noreply.github.com>
2023-12-21 21:43:43 +0530

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -6,8 +6,6 @@ from states import states_list, states_dict
 import pandas as pd
 import plotly.express as px
 
-# st.set_page_config(layout='wide')
-
 myconnection = pymysql.connect(host='127.0.0.1', user='root', passwd='MySQL@123', database='Phonepe')
 cur = myconnection.cursor()
 cur.execute("use Phonepe")
@@ -42,7 +40,7 @@ def basicInsights():
             quarter_selected = st.selectbox("Selected Quarter", [1, 2, 3, 4])
 
     if selected and table == 'Transaction' and year_selected and quarter_selected:
-        st.success("Your Insight")
+        st.success(f"Insight for the Year {year_selected}, Q{quarter_selected}, {selected}")
         df_1 = f"select * from map_transaction where state='{states_dict[selected][0]}' and year={year_selected} and Quater = {quarter_selected}"
         df_2 = f"select * from top_transaction_dist where state='{states_dict[selected][0]}' and year={year_selected} and Quater = {quarter_selected}"
         df_3 = f"select * from top_transaction_pin where state='{states_dict[selected][0]}' and year={year_selected} and Quater = {quarter_selected}"
@@ -50,17 +48,14 @@ def basicInsights():
         q_df_1 = pd.read_sql_query(df_1, engine)
         q_df_1['State'] = q_df_1['State'].str.replace('-', ' ').str.title()
         q_df_1['Transaction Count'] = q_df_1['Transacion_count'].apply(lambda x: '{:,.0f}'.format(x))
-        q_df_1['Transaction Amount'] = q_df_1['Transacion_amount'].apply(lambda x: '₹ {:,.0f}'.format(x))
+        q_df_1['Transaction Amounts'] = q_df_1['Transacion_amount'].apply(lambda x: '₹ {:,.0f}'.format(x))
 
         q_df_2 = pd.read_sql_query(df_2, engine)
         q_df_2['District_Name'] = q_df_2['District_Name'].str.title()
-        q_df_2['Transaction Count'] = q_df_2['Transacion_count'].apply(lambda x: '{:,.0f}'.format(x))
-        q_df_2['Transaction Amount'] = q_df_2['Transacion_amount'].apply(lambda x: '₹ {:,.0f}'.format(x))
 
         q_df_3 = pd.read_sql_query(df_3, engine)
-        q_df_3['Pin_Code'] = q_df_3['Pin_Code'].astype('object')
         q_df_3['Transaction Count'] = q_df_3['Transacion_count'].apply(lambda x: '{:,.0f}'.format(x))
-        q_df_3['Transaction Amount'] = q_df_3['Transacion_amount'].apply(lambda x: '₹ {:,.0f}'.format(x))
+        q_df_3['Transaction Amounts'] = q_df_3['Transacion_amount'].apply(lambda x: '₹ {:,.0f}'.format(x))
 
         t_col1, t_col2 = st.columns(2)
         with t_col1:
@@ -69,37 +64,59 @@ def basicInsights():
                                    color='District_Name'))
         with t_col2:
             st.subheader(":blue[Top 10 Districts]")
-            st.plotly_chart(px.pie(q_df_2, values='Transacion_count', names='District_Name',
-                                   color_discrete_sequence=px.colors.diverging.RdYlGn))
+            fig = px.pie(q_df_2, values='Transacion_count', names='District_Name',
+                         color_discrete_sequence=px.colors.diverging.RdYlGn)
+
+            fig.update_traces(
+                text=q_df_2['District_Name'],
+                textposition='inside',
+                hovertemplate='Transaction Count<br> %{value:,.0f}')
+            st.plotly_chart(fig)
 
         st.divider()
 
         u_col1, u_col2 = st.columns(2)
         with u_col1:
             st.subheader(":blue[Transaction Amount]")
-            st.plotly_chart(px.bar(q_df_1, x='District_Name', y='Transaction Amount', text='Transaction Amount',
+            st.plotly_chart(px.bar(q_df_1, x='District_Name', y='Transacion_amount', text='Transaction Amounts',
                                    color='District_Name'))
         with u_col2:
             st.subheader(":blue[Top 10 Districts]")
-            st.plotly_chart(px.pie(q_df_2, values='Transacion_amount', names='District_Name',
-                                   color_discrete_sequence=px.colors.diverging.RdYlGn))
+            fig1 = px.pie(q_df_2, values='Transacion_amount', names='District_Name',
+                          color_discrete_sequence=px.colors.diverging.RdYlGn)
+            fig1.update_traces(
+                text=q_df_2['District_Name'],
+                textposition='inside',
+                hovertemplate='Transaction Amount<br> ₹%{value:,.0f}')
+
+            st.plotly_chart(fig1)
 
         st.divider()
 
         p_col1, p_col2 = st.columns(2)
         with p_col1:
             st.subheader(":blue[Top 10 Pincodes for Transaction Count]")
-            st.plotly_chart(px.pie(q_df_3, values='Transacion_count', names='Pin_Code',
-                                   color_discrete_sequence=px.colors.diverging.RdYlGn))
+            fig2 = px.pie(q_df_3, values='Transacion_count', names='Pin_Code',
+                          color_discrete_sequence=px.colors.diverging.RdYlGn)
+            fig2.update_traces(
+                text=q_df_3['Pin_Code'],
+                textposition='inside',
+                hovertemplate='Transaction Count<br> %{value:,.0f}')
+            st.plotly_chart(fig2)
         with p_col2:
             st.subheader(":blue[Top 10 Pincodes for Transaction Amount]")
-            st.plotly_chart(px.pie(q_df_3, values='Transacion_amount', names='Pin_Code',
-                                   color_discrete_sequence=px.colors.diverging.RdYlGn))
+            fig3 = px.pie(q_df_3, values='Transacion_amount', names='Pin_Code',
+                          color_discrete_sequence=px.colors.diverging.RdYlGn)
+            fig3.update_traces(
+                text=q_df_3['Pin_Code'],
+                textposition='inside',
+                hovertemplate='Transaction Amount<br> ₹%{value:,.0f}')
+            st.plotly_chart(fig3)
 
         st.divider()
 
     elif selected and table == 'User' and year_selected and quarter_selected:
-        st.success("Your Insight")
+        st.success(f"Insight for the Year {year_selected}, Q{quarter_selected}, {selected}")
         df_1 = f"select * from map_users where state='{states_dict[selected][0]}' and year={year_selected} and Quater = {quarter_selected}"
         df_2 = f"select * from district_wise_users where state='{states_dict[selected][0]}' and year={year_selected} and Quater = {quarter_selected}"
         df_3 = f"select * from pincode_wise_users where state='{states_dict[selected][0]}' and year={year_selected} and Quater = {quarter_selected}"
@@ -111,6 +128,7 @@ def basicInsights():
         q_df_2['District_Name'] = q_df_2['District_Name'].str.title()
         q_df_3 = pd.read_sql_query(df_3, engine)
         q_df_4 = pd.read_sql_query(df_4, engine)
+        q_df_4['Brandwise_Registers'] = q_df_4['Brandwise_Register'].apply(lambda x: '{:,.0f}'.format(x))
 
         t_col1, t_col2 = st.columns(2)
         with t_col1:
@@ -119,9 +137,13 @@ def basicInsights():
                                    color='District_Name'))
         with t_col2:
             st.subheader(":blue[Top 10 Districts]")
-            st.plotly_chart(px.pie(q_df_2, values='Registered_User', names='District_Name',
-                                   color_discrete_sequence=px.colors.diverging.RdYlGn))
-
+            fig = px.pie(q_df_2, values='Registered_User', names='District_Name',
+                         color_discrete_sequence=px.colors.diverging.RdYlGn)
+            fig.update_traces(
+                text=q_df_2['District_Name'],
+                textposition='inside',
+                hovertemplate='Registered User<br> %{value:,.0f}')
+            st.plotly_chart(fig)
         st.divider()
 
         u_col1, u_col2 = st.columns(2)
@@ -136,17 +158,28 @@ def basicInsights():
             with u_col2:
                 st.subheader(":blue[Top 10 Districts]")
                 q_df_1.sort_values('App_Opens', ascending=True, inplace=True)
-                st.plotly_chart(px.pie(q_df_1.head(10), values='App_Opens', names='District_Name',
-                                       color_discrete_sequence=px.colors.diverging.RdYlGn))
+                q = q_df_1.tail(10)
+                fig1 = px.pie(q, values='App_Opens', names='District_Name',
+                              color_discrete_sequence=px.colors.diverging.RdYlGn)
+                fig1.update_traces(
+                    text=q['District_Name'],
+                    textposition='inside',
+                    hovertemplate='App Open<br> %{value:,.0f}')
+
+                st.plotly_chart(fig1)
 
         st.divider()
 
         st.subheader(":blue[Top 10 Pincodes for Registration Count]")
         p_col1, p_col2 = st.columns([1, 3])
         with p_col2:
-
-            st.plotly_chart(px.pie(q_df_3, values='Registered_User', names='Pin_Code',
-                                   color_discrete_sequence=px.colors.diverging.RdYlGn))
+            fig2 = px.pie(q_df_3, values='Registered_User', names='Pin_Code',
+                                   color_discrete_sequence=px.colors.diverging.RdYlGn)
+            fig2.update_traces(
+                text=q_df_3['Pin_Code'],
+                textposition='inside',
+                hovertemplate='Registered User<br> %{value:,.0f}')
+            st.plotly_chart(fig2)
 
         st.divider()
         if year_selected == 2023:
@@ -165,7 +198,6 @@ def performance():
     state_df = pd.DataFrame({'State Name': states_list, 'State Code': state_codes})
     df = pd.DataFrame()
 
-    st.write("### :blue[Performance over the years]")
     tab = st.selectbox('Select Overall Changes', ['Transaction Count', 'Transaction Amount', 'Registered User'])
     if tab == 'Transaction Count' or tab == 'Transaction Amount':
         df = pd.read_sql_query('select state, year, sum(transacion_count) `Transaction Count`, sum(transacion_amount) '
@@ -178,6 +210,8 @@ def performance():
 
     state = df.merge(state_df, left_on='state', right_on='State Name', how='left')
     state['State Name'] = state['State Code'] + '-' + state['State Name']
+
+    st.subheader(f':blue[Performance of the PhonePe over the years, {tab}]')
 
     fig = px.bar(
         state,
@@ -212,30 +246,37 @@ def top():
             f'select state, year, sum(transacion_count) `Transaction Count` from aggregated_transaction where year = {year} group by 1, 2 order by `Transaction Count` desc limit 10',
             engine)
         df['state'] = df['state'].str.replace('-', ' ').str.title()
+        df['Transaction Count'] = df['Transaction Count'].apply(lambda x: '{:,.0f}'.format(x))
         d_df = pd.read_sql_query(
             f'select district_name, year, sum(transacion_count) `Transaction Count` from map_transaction where year = {year} group by 1, 2 order by `Transaction Count` desc limit 10',
             engine)
         d_df['district_name'] = d_df['district_name'].str.title()
+        d_df['Transaction Count'] = d_df['Transaction Count'].apply(lambda x: '{:,.0f}'.format(x))
         p_df = pd.read_sql_query(
             f'select pin_code, year, sum(transacion_count) `Transaction Count` from top_transaction_pin where year = {year} group by 1, 2 order by `Transaction Count`desc limit 10',
             engine)
+        p_df['Transaction Count'] = p_df['Transaction Count'].astype(int)
 
-        st.subheader(f':blue[Top {df["state"].count()} States of the year {year}]')
+        st.subheader(f':blue[Top 10 States of the year {year}, {select}]')
         col1, col2 = st.columns([1, 3])
         with col2:
             st.plotly_chart(px.bar(df, x='state', y='Transaction Count', color='state', text='Transaction Count'))
         st.divider()
-        st.subheader(f':blue[Top {d_df["district_name"].count()} Districts of the year {year}]')
+        st.subheader(f':blue[Top 10 Districts of the year {year}, {select}]')
         col3, col4 = st.columns([1, 3])
         with col4:
             st.plotly_chart(
                 px.bar(d_df, x='district_name', y='Transaction Count', color='district_name', text='Transaction Count'))
         st.divider()
-        st.subheader(f':blue[Top 10 Pincode of the year {year}]')
+        st.subheader(f':blue[Top 10 Pincode of the year {year}, {select}]')
         col5, col6 = st.columns([1, 3])
         with col6:
-            fig = px.pie(p_df,  values='Transaction Count', names='pin_code',
-                                       color_discrete_sequence=px.colors.diverging.RdYlGn)
+            fig = px.pie(p_df, values='Transaction Count', names='pin_code', hover_name='pin_code',
+                         color_discrete_sequence=px.colors.diverging.RdYlGn)
+            fig.update_traces(
+                text=p_df['pin_code'],
+                textposition='inside',
+                hovertemplate='Transaction Count<br> %{value:,.0f}')
             st.plotly_chart(fig)
 
 
@@ -244,31 +285,38 @@ def top():
                                f'sum(transacion_amount) `Transaction Amount` from aggregated_transaction where year = {year} group by 1, 2 order by `Transaction Amount` desc limit 10',
                                engine)
         df['state'] = df['state'].str.replace('-', ' ').str.title()
+        df['Transaction Amounts'] = df['Transaction Amount'].apply(lambda x: '₹{:,.0f}'.format(x))
         d_df = pd.read_sql_query(
             f'select district_name, year, sum(transacion_amount) `Transaction Amount` from map_transaction where year = {year} group by 1, 2 order by `Transaction Amount` desc limit 10',
             engine)
         d_df['district_name'] = d_df['district_name'].str.title()
+        d_df['Transaction Amounts'] = df['Transaction Amount'].apply(lambda x: '₹{:,.0f}'.format(x))
         p_df = pd.read_sql_query(
-            f'select pin_code, year, sum(transacion_amount) `Transaction Amount` from top_transaction_pin where year = {year} group by 1, 2 order by `Transaction Amount` desc limit 10',
+            f'select pin_code, year, round(sum(transacion_amount)) `Transaction Amount` from top_transaction_pin where year = {year} group by 1, 2 order by `Transaction Amount` desc limit 10',
             engine)
+        p_df['formatted_value'] = p_df['Transaction Amount'].apply(lambda x: f'{x:,.0f}')
 
-        st.subheader(f':blue[Top {df["state"].count()} States of the year {year}]')
+        st.subheader(f':blue[Top 10 States of the year {year}, {select}]')
         col1, col2 = st.columns([1, 3])
         with col2:
-            st.plotly_chart(px.bar(df, x='state', y='Transaction Amount', color='state', text='Transaction Amount'))
+            st.plotly_chart(px.bar(df, x='state', y='Transaction Amount', color='state', text='Transaction Amounts'))
         st.divider()
 
-        st.subheader(f':blue[Top {d_df["district_name"].count()} Districts of the year {year}]')
+        st.subheader(f':blue[Top 10 Districts of the year {year}, {select}]')
         col3, col4 = st.columns([1, 3])
         with col4:
             st.plotly_chart(px.bar(d_df, x='district_name', y='Transaction Amount', color='district_name',
-                                   text='Transaction Amount'))
+                                   text='Transaction Amounts'))
         st.divider()
-        st.subheader(f':blue[Top 10 Pincode of the year {year}]')
+        st.subheader(f':blue[Top 10 Pincode of the year {year}, , {select}]')
         col5, col6 = st.columns([1, 3])
         with col6:
-            fig = px.pie(p_df,  values='Transaction Amount', names='pin_code',
-                                       color_discrete_sequence=px.colors.diverging.RdYlGn)
+            fig = px.pie(p_df, values='Transaction Amount', names='pin_code', hover_name='pin_code',
+                         color_discrete_sequence=px.colors.diverging.RdYlGn)
+            fig.update_traces(
+                text=p_df['pin_code'],
+                textposition='inside',
+                hovertemplate='Transaction Amount<br> ₹%{value:,.0f}')
             st.plotly_chart(fig)
 
     elif select == 'Registered User':
@@ -276,42 +324,58 @@ def top():
             f'select state, year, sum(registered_user) `Registered User` from aggregated_user where year = {year} group by 1, 2 order by `Registered User` desc limit 10',
             engine)
         df['state'] = df['state'].str.replace('-', ' ').str.title()
+        df['Registered User'] = df['Registered User'].apply(lambda x: '{:,.0f}'.format(x))
         d_df = pd.read_sql_query(
             f'select district_name, year, sum(registered_user) `Registered User` from map_users where year = {year} group by 1, 2 order by `Registered User` desc limit 10',
             engine)
         d_df['district_name'] = d_df['district_name'].str.title()
+        d_df['Registered User'] = d_df['Registered User'].apply(lambda x: '{:,.0f}'.format(x))
         p_df = pd.read_sql_query(
             f'select pin_code, year, sum(registered_user) `Registered User` from pincode_wise_users where year = {year} group by 1, 2 order by `Registered User` desc limit 10',
             engine)
+        p_df['Registered User'] = p_df['Registered User'].astype(int)
 
-        st.subheader(f':blue[Top {df["state"].count()} States of the {year}]')
+        st.subheader(f':blue[Top 10 States of the {year}, {select}]')
         col1, col2 = st.columns([1, 3])
         with col2:
             st.plotly_chart(px.bar(df, x='state', y='Registered User', color='state', text='Registered User'))
         st.divider()
-        st.subheader(f':blue[Top {d_df["district_name"].count()} Districts of the year {year}]')
+        st.subheader(f':blue[Top 10 Districts of the year {year}, {select}]')
         col3, col4 = st.columns([1, 3])
         with col4:
             st.plotly_chart(
                 px.bar(d_df, x='district_name', y='Registered User', color='district_name', text='Registered User'))
         st.divider()
-        st.subheader(f':blue[Top 10 Pincode of the year {year}]')
+        st.subheader(f':blue[Top 10 Pincode of the year {year}, {select}]')
         col5, col6 = st.columns([1, 3])
         with col6:
-            fig = px.pie(p_df,  values='Registered User', names='pin_code',
-                                       color_discrete_sequence=px.colors.diverging.RdYlGn)
+            fig = px.pie(p_df, values='Registered User', names='pin_code', hover_name='pin_code',
+                         color_discrete_sequence=px.colors.diverging.RdYlGn)
+            fig.update_traces(
+                text=p_df['pin_code'],
+                textposition='inside',
+                hovertemplate='Registered User<br> %{value:,.0f}')
             st.plotly_chart(fig)
 
     elif select == 'Payment Type':
         df = pd.read_sql_query(
-            f'select transacion_type `Transaction Type`, year, sum(Transacion_count) `Transaction Count`, sum(Transacion_amount) `Transacion Amount` from aggregated_transaction where year ={year} group by 1, 2',
+            f'select transacion_type `Transaction Type`, year, sum(Transacion_count) `Transaction Count`, sum(Transacion_amount) `Transaction Amount` from aggregated_transaction where year ={year} group by 1, 2',
             engine)
+        df['Transaction Count'] = df['Transaction Count'].apply(lambda x: '{:,.0f}'.format(x))
+        df['Transaction Amounts'] = df['Transaction Amount'].apply(lambda x: '₹{:,.0f}'.format(x))
 
-        st.subheader(f':blue[Transaction Types]')
+        st.subheader(f':blue[Transaction Types of {year} - Transaction Counts]')
         col1, col2 = st.columns([1, 3])
         with col2:
             st.plotly_chart(px.bar(df, x='Transaction Type', y='Transaction Count', color='Transaction Type',
                                    text='Transaction Count'))
+        st.divider()
+
+        st.subheader(f':blue[Transaction Types {year} - Transaction Amount]')
+        col3, col4 = st.columns([1, 3])
+        with col4:
+            st.plotly_chart(px.bar(df, x='Transaction Type', y='Transaction Amount', color='Transaction Type',
+                                   text='Transaction Amounts'))
 
 
 def visualisation():
@@ -322,4 +386,5 @@ def visualisation():
         performance()
     with tab3:
         top()
+
 
